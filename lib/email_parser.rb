b1f5ef1e5a5e7d87475597f8536295e8472c13e5
Cindy Cai <caicindy87@gmail.com>
2020-03-11 00:06:36 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -5,8 +5,12 @@
 
 class EmailAddressParser
   attr_accessor :emails
-  
-  def parse
-    @emails.split(/ /)
-  end
+    
+    def initialize(emails)
+        @emails = emails
+    end
+
+    def parse
+      @emails.split(/ /)
+    end
 end
